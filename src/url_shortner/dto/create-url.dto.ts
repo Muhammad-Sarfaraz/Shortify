42a480f125f819b74283b7954b2ea6aea8f54ac9
Sarfaraz Muhammad Sajib <id.sent.me@gmail.com>
2024-05-26 23:23:39 +0600

    add global prefix

--- src/url_shortner/dto/create-url.dto.ts
+++ src/url_shortner/dto/create-url.dto.ts
@@ -10,16 +10,7 @@ import {
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateURLDto {
-  @IsNotEmpty()
-  @IsString({ message: 'short url must be a text' })
-  @ApiProperty()
-  short_url: string;
-
   @IsString({ message: 'long url must be a text' })
   @ApiProperty()
   long_url: string;
-
-  @IsString({ message: 'description must be a text' })
-  @ApiProperty()
-  description: string;
 }
